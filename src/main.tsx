fefdd6928077e6965fd4c34965c5d0f848dcf973
detstvo <95248947+meePwn1@users.noreply.github.com>
2023-12-14 16:19:15 +0500

    homework ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,16 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './s1-main/App'
+import store from './s2-homeworks/hw10/bll/store'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(
+	<React.StrictMode>
+		{/*для дз 10*/}
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+)
